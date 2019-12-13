2e045dc7dba0f168f0ed46a9e6b1372151b08cf4
Robert Tanase <robert@crowdconnected.com>
2019-12-13 16:29:36 +0000

    Added more test cases

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 })
 
-var messages = require('./ClientMessagingProtocol_pb');
+var messages = require('./MessagingProtocol_pb');
 
 const testCases = {
   STOPLIBRARY: 'StopLibraryRemotely',
   GEOINCYCLES: 'GeoLocationsInCycles',
   GEOCONTINUOUSLY: 'GeoLocationsContinuously',
+  ACCURACYANDDISTANCE: 'GeoAccuracyAndDistance',
+  SIGNIFICANTUPDATES: 'GeoSignificantUpdates',
+  GEOFENCEEVENT: 'GeofenceEvent',
   UNKNOWN: 'Unknown'
 }
 
@@ -39,7 +42,13 @@ function detectTestCaseAndStart(commandString) {
     testCase = testCases.GEOINCYCLES;
   } else if (commandString.includes("GeoLocationsContinuously")) {
     testCase = testCases.GEOCONTINUOUSLY;
-  } 
+  } else if (commandString.includes("GeoAccuracyAndDistance")) {
+    testCase = testCases.ACCURACYANDDISTANCE;
+  } else if (commandString.includes("GeoSignificantUpdates")) {
+    testCase = testCases.SIGNIFICANTUPDATES;
+  } else if (commandString.includes("GeofenceEvent")) {
+    testCase = testCases.GEOFENCEEVENT;
+  }
   
   console.log("\n@@@ Test case detected: ", testCase);
   startTest(testCase);
@@ -53,8 +62,14 @@ function startTest(testCase) {
     client.send(serrverSettings.serializeBinary());
   })
 
+  startTrackingTestProgress(testCase)
+}
+
+function startTrackingTestProgress(testCase) {
   if (testCase == testCases.GEOINCYCLES) {
     updateLastMessagesNumber();
+    successTestCase = true;
+
     checkMessagesNumber(5000, true);
     checkMessagesNumber(10000, false);
     checkMessagesNumber(39000, true);
@@ -62,6 +77,7 @@ function startTest(testCase) {
     checkSuccessTestCase(40000)
   } else if (testCase == testCases.GEOCONTINUOUSLY) {
     updateLastMessagesNumber();
+    successTestCase = true;
 
     checkMessagesNumber(5000, true);
     checkMessagesNumber(15000, true);
@@ -70,12 +86,35 @@ function startTest(testCase) {
     checkSuccessTestCase(30000)
   } else if (testCase == testCases.STOPLIBRARY) {
     updateLastMessagesNumber();
+    successTestCase = true;
 
     checkMessagesNumber(5000, false);
     checkMessagesNumber(10000, false);
     checkMessagesNumber(19000, false);
 
     checkSuccessTestCase(20000)
+  } else if (testCase == testCases.ACCURACYANDDISTANCE) {
+    updateLastMessagesNumber();
+    successTestCase = true;
+
+    checkMessagesFlowAfterSeconds(20000);
+    checkSuccessTestCase(21000);
+  } else if (testCase == testCases.SIGNIFICANTUPDATES) {
+    updateLastMessagesNumber();
+    successTestCase = true;
+
+    checkMessagesNumber(31000, true);
+    checkSuccessTestCase(32000);
+  } else if (testCase == testCases.GEOFENCEEVENT) {
+    updateLastMessagesNumber();
+    successTestCase = true;
+
+    // send geofence with cupertino coordinates and big radius
+    // start test when app has just started
+    // you shouldn't track location (probably)
+    // check if you get a message when the app exit the geofence
+    // is possible that first event not to be reported
+    
   }
 }
 
@@ -149,6 +188,78 @@ let getServerSettingsForTestCase = function(testCase) {
         serverMessage.setGlobalsettings(globalSettings);
         serverMessage.setIossettings(iosSettings);
         return serverMessage;
+    case testCases.ACCURACYANDDISTANCE:
+        globalSettings.setRadiosilence(0);
+  
+        foregroundGeoSettings.setDesiredaccuracy(-1);
+        foregroundGeoSettings.setDistancefilter(-1);
+        foregroundGeoSettings.setPausesupdates(false);
+
+        geoSettings.setForegroundgeo(foregroundGeoSettings)
+        geoSettings.setBackgroundgeo(backgroundGeoSettings)
+        geoSettings.setSignificantupates(true);
+
+        beaconSettings.setMonitoring(beaconMonitoringSettings)
+        beaconSettings.setForegroundranging(foregrounfBeaconRanging);
+        beaconSettings.setBackgroundranging(backgrounfBeaconRanging);
+    
+        iosSettings.setGeosettings(geoSettings);
+        iosSettings.setBeaconsettings(beaconSettings);
+    
+        serverMessage.setGlobalsettings(globalSettings);
+        serverMessage.setIossettings(iosSettings);
+        return serverMessage;
+    case testCases.SIGNIFICANTUPDATES:
+        globalSettings.setRadiosilence(0);
+  
+        foregroundGeoSettings.setDistancefilter(10000);
+
+        geoSettings.setForegroundgeo(foregroundGeoSettings)
+        geoSettings.setBackgroundgeo(backgroundGeoSettings)
+        geoSettings.setSignificantupates(true);
+
+        beaconSettings.setMonitoring(beaconMonitoringSettings)
+        beaconSettings.setForegroundranging(foregrounfBeaconRanging);
+        beaconSettings.setBackgroundranging(backgrounfBeaconRanging);
+    
+        iosSettings.setGeosettings(geoSettings);
+        iosSettings.setBeaconsettings(beaconSettings);
+    
+        serverMessage.setGlobalsettings(globalSettings);
+        serverMessage.setIossettings(iosSettings);
+        return serverMessage;
+    case testCases.GEOFENCEEVENT:
+        globalSettings.setRadiosilence(0);
+
+        foregroundGeoSettings.setDesiredaccuracy(100);
+        foregroundGeoSettings.setDistancefilter(100000);
+        foregroundGeoSettings.setPausesupdates(false);
+
+        geoSettings.setForegroundgeo(foregroundGeoSettings)
+        geoSettings.setBackgroundgeo(backgroundGeoSettings)
+        geoSettings.setSignificantupates(false);
+
+        beaconSettings.setMonitoring(beaconMonitoringSettings)
+        beaconSettings.setForegroundranging(foregrounfBeaconRanging);
+        beaconSettings.setBackgroundranging(backgrounfBeaconRanging);
+
+// Coordinates for Freeway Ride, Standford, CA, US (next to Cupertino)
+// 37.403509, -122.186210
+
+        var circularGeofence= new messages.IosCircularGeoFence();
+        circularGeofence.setLatitude(37.403509);
+        circularGeofence.setLongitude(-122.186210);
+        circularGeofence.setRadius(20000);
+
+        var geofencesArray = [circularGeofence];
+
+        geoSettings.setIoscirculargeofencesList(geofencesArray);
+        iosSettings.setGeosettings(geoSettings);
+        iosSettings.setBeaconsettings(beaconSettings);
+    
+        serverMessage.setGlobalsettings(globalSettings);
+        serverMessage.setIossettings(iosSettings);
+        return serverMessage;
     case testCases.UNKNOWN:
         return serverMessage;
   }
@@ -182,10 +293,16 @@ function updateLastMessagesNumber() {
   }, 1000);
 }
 
-
-// Add tests for geofence update, geofence event, significant updates, desired accuracy and distance (the flow/ debit of messages comming in)
-
-
-// Coordinates for Freeway Ride, Standford, CA, US (next to Cupertino)
-// 37.403509, -122.186210
-// The ra
+function checkMessagesFlowAfterSeconds(seconds) {
+  // average debit is 1 update/sec having the simultaor on Freeway Drive and settings -1, -1
+  let messagesPerSec = 1;
+  let debit = messagesPerSec * seconds / 1000;
+  let errorMargin = debit * 0.9;
+  setTimeout(function() { 
+    if (receivedMessages - lastMessagesNumber < debit - errorMargin) {
+       console.log("The expected number of messages in ", seconds/1000, " is minimum", debit - errorMargin , " and the number of messages received is", receivedMessages - lastMessagesNumber);
+       successTestCase = false
+    }
+    lastMessagesNumber = receivedMessages;
+  }, seconds);
+}
